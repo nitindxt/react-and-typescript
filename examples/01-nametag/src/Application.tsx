e3f81c918480461ccc86d69ea26777338fdccdc5
Nitin Dixit <nitinajaydixit@gmail.com>
2022-05-20 20:34:07 +0530

    add prettier

--- examples/01-nametag/src/Application.tsx
+++ examples/01-nametag/src/Application.tsx
@@ -1,26 +1,27 @@
 /* added types */
 
 type GreetingProps = {
-    name: string;
-    greetingText?: string;
-}
+  name: string;
+  greetingText?: string;
+};
 
-const NameTag = ({name, greetingText="Hello !"}:GreetingProps) => {
-    return (
-      <main>
-        <header>
-          <h1>{greetingText}</h1>
-          <p>my name is</p>
-        </header>
-        <section className="display-name">
-          <p>{name}</p>
-        </section>
-        <footer />
-      </main>
-    );
-  };
-  
-  const Application = () => <NameTag name="Nitin Dixit" greetingText="Good Evening!" />;
-  
-  export default Application;
-  
+const NameTag = ({ name, greetingText = "Hello !" }: GreetingProps) => {
+  return (
+    <main>
+      <header>
+        <h1>{greetingText}</h1>
+        <p>my name is</p>
+      </header>
+      <section className="display-name">
+        <p>{name}</p>
+      </section>
+      <footer />
+    </main>
+  );
+};
+
+const Application = () => (
+  <NameTag name="Nitin Dixit" greetingText="Good Evening!" />
+);
+
+export default Application;
