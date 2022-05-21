a38f8af0b36f7fe48b5c47717daac2178c77339e
Nitin Dixit <nitinajaydixit@gmail.com>
2022-05-21 07:33:08 +0530

    type declare children with optional styling

---
+++ examples/02-typing-children-with-styling/src/App.tsx
@@ -0,0 +1,25 @@
+type BoxProps = { children: React.ReactNode; style?: React.CSSProperties };
+
+const Box = ({ children, style = {} }: BoxProps) => {
+  return (
+    <section style={{ padding: "1em", border: "5px solid purple", ...style }}>
+      {children}
+    </section>
+  );
+};
+
+export const App = () => {
+  return (
+    <Box>
+      Just a string.
+      <p>Some HTML that is not nested.</p>
+      <Box style={{ borderColor: "blueviolet" }}>
+        <h2>Another React component with one child.</h2>
+      </Box>
+      <Box>
+        <h2>A nested React component with two children.</h2>
+        <p>The second child.</p>
+      </Box>
+    </Box>
+  );
+};
--- examples/02-typing-children-with-styling/src/main.tsx
+++ examples/02-typing-children-with-styling/src/main.tsx
@@ -1,6 +1,6 @@
-import Application from "./Application";
 import { createRoot } from "react-dom/client";
-import "./style.css";
+import { App } from "./App";
+
 
 const rootElement = createRoot(document.getElementById("root")!); //* ! = a non-null assertion operator in ts
-rootElement.render(<Application />);
+rootElement.render(<App />);
