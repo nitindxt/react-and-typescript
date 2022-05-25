b8762a34fb5aa89871f636b02e165cacf4d1511e
Nitin Dixit <nitinajaydixit@gmail.com>
2022-05-25 10:03:07 +0530

    refactor with explicit return types, explicit handler func

--- examples/10-incident-counter-with-reducer/src/App.tsx
+++ examples/10-incident-counter-with-reducer/src/App.tsx
@@ -8,7 +8,7 @@ type state = {
   count: number;
 };
 
-const countReducer = (state: state, action: actions) => {
+const countReducer = (state: state, action: actions): state => {
   if (action.type === "Increment") {
     return {
       count: state.count + 1,
@@ -23,41 +23,33 @@ const countReducer = (state: state, action: actions) => {
 
   if (action.type === "Reset") {
     return {
-      count: (state.count = 0),
+      count: 0,
     };
   }
-
-  throw Error("Unknown action.");
+  return state; //this or 👇🏻
+  //throw Error("Unknown action.");
 };
 
 const Counter = () => {
   const [count, dispatch] = useReducer(countReducer, { count: 0 });
+  const increment = () => {
+    dispatch({ type: "Increment" });
+  };
+  const reset = () => {
+    dispatch({ type: "Reset" });
+  };
+  const decrement = () => {
+    dispatch({ type: "Decrement" });
+  };
+
   return (
     <main className="Counter">
       <h1>Days Since Last Incident</h1>
       <p className="count">{count.count}</p>
       <section className="controls">
-        <button
-          onClick={() => {
-            dispatch({ type: "Increment" });
-          }}
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => {
-            dispatch({ type: "Reset" });
-          }}
-        >
-          Reset
-        </button>
-        <button
-          onClick={() => {
-            dispatch({ type: "Decrement" });
-          }}
-        >
-          Decrement
-        </button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={decrement}>Decrement</button>
       </section>
     </main>
   );
